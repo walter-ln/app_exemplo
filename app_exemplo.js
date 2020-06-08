4ffadd5270de80d9876aaca184b514b8a1bf6421
Walter Lopes Neto <walter@bravo.local>
2020-06-08 18:44:44 -0300

    "Adição inicial de código principal  do app"

---
+++ app_exemplo.js
@@ -0,0 +1,17 @@
+console.log("Primeiro app em node.js")
+
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('Página prncipal!\n');
+});
+
+app.get('/Contatos', function (req, res) {
+  res.send('Página de Contatos!\n');
+});
+
+app.listen(8080, function () {
+  console.log('Aplicaçõe escutando na porta 8080!');
+});
+
